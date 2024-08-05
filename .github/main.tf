157cf667815b8eb3827eafcb42a776b40b945f72
maxoba <maxoba@yahoo.com>
2024-08-05 10:46:33 -0600

    22 commit

--- .github/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {}
-
-resource "aws_vpc" "action-test" {
-    cidr_block= "10.0.0.0/24"
-    tags ={
-      name="action-test"  
-    }
-}
---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {}
+
+resource "aws_vpc" "action-test" {
+    cidr_block= "10.0.0.0/24"
+    tags ={
+      name="action-test"  
+    }
+}
